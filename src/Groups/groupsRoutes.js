c94dfefaccde545ec698b4248c7d6bdb4f321c0c
mikeolivotto <michaelolivotto@gmail.com>
2021-12-06 16:11:25 +1100

    Set up http methods

--- src/Groups/groupsRoutes.js
+++ src/Groups/groupsRoutes.js
@@ -4,4 +4,16 @@ const routes = express.Router();
 
 routes.get("/", async (request, response) => {});
 
+routes.post("/", async (request, response) => {
+    response.json({ message: "POST - group created" });
+});
+
+routes.put("/", async (request, response) => {
+    response.json({ message: "PUT - group edited" });
+});
+
+routes.delete("/", async (request, response) => {
+    response.json({ message: "DELETE - group deleted" });
+});
+
 module.exports = routes;
--- src/Profiles/profilesRoutes.js
+++ src/Profiles/profilesRoutes.js
@@ -5,4 +5,16 @@ routes.get("/", async (request, response) => {
   response.json({ message: "This is all the profiles" });
 });
 
+routes.post("/", async (request, response) => {
+  response.json({ message: "POST - profile created" });
+});
+
+routes.put("/", async (request, response) => {
+  response.json({ message: "PUT - profile edited" });
+});
+
+routes.delete("/", async (request, response) => {
+  response.json({ message: "DELETE - profile deleted" });
+});
+
 module.exports = routes;
