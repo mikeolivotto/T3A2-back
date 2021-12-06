46c8ebd1690cee0e4de3519294f7f2e3558a7350
malanchristiansen <malanschristiansen@gmail.com>
2021-12-06 15:59:07 +1100

    setting up routes for profile and home

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-
 // import and initialize express as app.
 const express = require("express");
 
 const app = express();
 
-PORT = process.env.PORT || 3000
-HOST = "0.0.0.0"
+PORT = process.env.PORT || 3000;
+HOST = "0.0.0.0";
 
+app.get("/", (request, response) => {
+  response.json({ message: "This is the home route" });
+});
 
-const importedProfileRouting = require("./Profiles/profilesRoutes")
-app.use("/Profiles", importedProfileRouting)
+const importedProfileRouting = require("./Profiles/profilesRoutes");
+app.use("/profiles", importedProfileRouting);
+
+const importedGroupsRouting = require("./Groups/groupsRoutes");
+app.use("/Groups", importedGroupsRouting);
+
+const importedGamesRouting = require("./Games/gamesRoutes");
+const { request } = require("express");
+app.use("/Games", importedGamesRouting);
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server running on PORT: ${PORT}`)
-})
+  console.log(`Server running on PORT: ${PORT}`);
+});
