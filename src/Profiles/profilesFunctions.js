c27c2487ba8aa75ddd92a534ecb9bb5ab26cb6dd
malanchristiansen <malanschristiansen@gmail.com>
2021-12-08 12:54:31 +1100

    fixed signup function with await

--- src/Profiles/profilesFunctions.js
+++ src/Profiles/profilesFunctions.js
@@ -5,31 +5,20 @@ firebaseClient.initializeApp(JSON.parse(process.env.CLIENT_KEY_FIREBASE));
 const firebaseAdmin = require("firebase-admin");
 
 async function signUpUser(userDetails) {
-  return (
-    firebaseAdmin
-      .auth()
-      .createUser({
-        email: userDetails.email,
-        password: userDetails.password,
-        emailVerified: true,
-      })
-      // .then(async (UserRecord) => {
-      //     let defaultUserClaims = firebaseAdmin
-      //       .auth()
-      //       .setCustomUserClaims(userRecord.uid, {
-      //         admin: false,
-      //         regularUser: true,
-      //       })
-      //       .then(() => {
-      //         console.log("Set default claims to new user");
-      //       });
-      //   return userRecord;
-      // })
-      .catch((error) => {
-        console.log(`Internal function error is: ${error}`);
-        return { error: error };
-      })
-  );
+  return firebaseAdmin
+    .auth()
+    .createUser({
+      email: userDetails.email,
+      password: userDetails.password,
+      emailVerified: true,
+    })
+    .then(async (UserRecord) => {
+      return UserRecord;
+    })
+    .catch((error) => {
+      console.log(`Internal function error is: ${error}`);
+      return { error: error };
+    });
 }
 
 async function createNewProfile(queryData) {
