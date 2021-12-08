a37ed583deddd36d4e449f2aa0f5b3240c54de2a
malanchristiansen <malanschristiansen@gmail.com>
2021-12-08 12:09:25 +1100

    setup firebase and created a signup route

--- src/Profiles/profilesFunctions.js
+++ src/Profiles/profilesFunctions.js
@@ -1,20 +1,53 @@
-const {Profile} = require("../database/schemas/profilesSchema");
+const { Profile } = require("../database/schemas/profilesSchema");
 
-async function createNewProfile(queryData){
-    let newProfile = new Profile({
-        username: queryData.username
-    });
-    let newProfileResult = await newProfile.save()
-    return newProfileResult
-};
+const firebaseClient = require("firebase/app");
+firebaseClient.initializeApp(JSON.parse(process.env.CLIENT_KEY_FIREBASE));
+const firebaseAdmin = require("firebase-admin");
 
+async function signUpUser(userDetails) {
+  return (
+    firebaseAdmin
+      .auth()
+      .createUser({
+        email: userDetails.email,
+        password: userDetails.password,
+        emailVerified: true,
+      })
+      // .then(async (UserRecord) => {
+      //     let defaultUserClaims = firebaseAdmin
+      //       .auth()
+      //       .setCustomUserClaims(userRecord.uid, {
+      //         admin: false,
+      //         regularUser: true,
+      //       })
+      //       .then(() => {
+      //         console.log("Set default claims to new user");
+      //       });
+      //   return userRecord;
+      // })
+      .catch((error) => {
+        console.log(`Internal function error is: ${error}`);
+        return { error: error };
+      })
+  );
+}
+
+async function createNewProfile(queryData) {
+  let newProfile = new Profile({
+    username: queryData.username,
+  });
+  let newProfileResult = await newProfile.save();
+  return newProfileResult;
+}
 
 // The ".exec()" helps the query just run instead of saving it for re-use.
-async function getSpecificProfile(profileID){
-    let specificProfileQuery = await Profile.findById(profileID).exec();
-    return specificProfileQuery;
+async function getSpecificProfile(profileID) {
+  let specificProfileQuery = await Profile.findById(profileID).exec();
+  return specificProfileQuery;
 }
 
-module.exports ={
-    createNewProfile, getSpecificProfile
-}
+module.exports = {
+  createNewProfile,
+  getSpecificProfile,
+  signUpUser,
+};
--- src/Profiles/profilesRoutes.js
+++ src/Profiles/profilesRoutes.js
@@ -1,29 +1,51 @@
 const express = require("express");
-const { createNewProfile, getSpecificProfile } = require("./profilesFunctions")
+const { createNewProfile, getSpecificProfile } = require("./profilesFunctions");
+
+const { signUpUser } = require("./profilesFunctions");
 const routes = express.Router();
+
+routes.post("/sign-up", async (request, response) => {
+  let newProfileDetails = {
+    email: request.body.email,
+    password: request.body.password,
+  };
+
+  let signUpResult = signUpUser(newProfileDetails);
+
+  if (signUpResult.error != null) {
+    console.log("Sign-up error");
+    response.json(signUpResult);
+    return;
+  }
+  response.json(signUpResult);
+});
 
 routes.get("/", async (request, response) => {
   response.json({ message: "GET - all profiles" });
 });
 
 routes.get("/:id", async (request, response) => {
-  let profileResult = await getSpecificProfile(request.params.id)
+  let profileResult = await getSpecificProfile(request.params.id);
   response.json(profileResult);
 });
 
 routes.post("/", async (request, response) => {
-    console.log("route matched post profile")
-    console.log(request.body)
-    let postResult = await createNewProfile(request.body)
+  console.log("route matched post profile");
+  console.log(request.body);
+  let postResult = await createNewProfile(request.body);
   response.json({ message: `POST - profile created: \n${postResult}` });
 });
 
 routes.put("/:id", async (request, response) => {
-  response.json({ message: `PUT - profile with id ${request.params.id} edited` });
+  response.json({
+    message: `PUT - profile with id ${request.params.id} edited`,
+  });
 });
 
 routes.delete("/:id", async (request, response) => {
-  response.json({ message: `DELETE - profile with id ${request.params.id} deleted` });
+  response.json({
+    message: `DELETE - profile with id ${request.params.id} deleted`,
+  });
 });
 
 module.exports = routes;
