6d298d9ec483dbd26cb236acf7ff990720575cd9
NicholasSaltis <gcas012107@coderacademy.edu.au>
2021-12-13 15:37:16 +1100

    added supertest, added testing folder with routes test file. edited index.js to set database connection based on NODE_ENV

---
+++ testing/routes.test.js
@@ -0,0 +1,17 @@
+const {app} = require("../src/index")
+const request = require("supertest");
+
+describe('Profiles routes', function() {
+    it(' GET / responds with json & status 200', function(done) {
+      request(app)
+        .get('/profiles')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+    it("GET /:id responds with json & status 200", (done) => {
+        request(app)
+            .get('/profiles')
+            .expect('Content-Type', /json/)
+            .expect(200, done); 
+    })
+  });
